fcba797355d30d0c43a0821dd548455a10e2e032
Kichinda34 <leandroschneiderlima@gmail.com>
2023-03-27 13:52:57 -0300

    Add project files.

---
+++ To_Do_List/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace To_Do_List
+{
+    internal class Person
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Person Person()
+        {
+            return new Person();
+        }
+
+        public Person SetName()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            
+        }
+    }
+}
---
+++ To_Do_List/ToDo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace To_Do_List
+{
+    internal class ToDo
+    {
+        public Guid Id { get; set; }
+
+        public string Description { get; set; }
+
+        public string Category { get; set; }
+
+        public Person Owner { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public bool Status { get; set; } 
+
+        public ToDo ToDo()
+        {
+
+        }
+
+        public string ToFile ()
+        {
+
+        }
+
+        public bool SetStatus()
+        {
+
+        }
+
+        public Person Person()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            
+        }
+    }
+}
